67d60917b92e10c41184f25378ad1992a2070490
treeplate <tree.plate.moo@gmail.com>
2020-12-31 19:59:02 -0800

    add lasers and improve packetting

--- lib/handler.dart
+++ lib/handler.dart
@@ -5,9 +5,10 @@ import 'package:flutter/cupertino.dart';
 Socket server;
 
 class WorldState {
-  WorldState(this.world, this.size);
+  WorldState(this.world, this.size, this.lasers);
   final Size size;
   final List<MapEntry<Position, CellType>> world;
+  final List<List<List<int>>> lasers;
 }
 
 class Position {
@@ -26,18 +27,35 @@ class Position {
 enum CellType { enemy, tower }
 
 WorldState parse(List<int> data) {
+  if(data.isEmpty) throw "NO DATA";
   int w = data[0];
   int h = data[1];
   int i = 2;
+  List<List<List<int>>> lasers = [];
   List<MapEntry<Position, CellType>> world = [];
   while (i < data.length) {
-    int x = data[i];
-    i++;
-    int y = data[i];
-    i++;
-    CellType type = CellType.values[data[i]];
-    i++;
-    world.add(MapEntry(Position(x, y), type));
+    if(data[i] == 0) {
+      i++;
+      int x = data[i];
+      i++;
+      int y = data[i];
+      i++;
+      CellType type = CellType.values[data[i]];
+      i++;
+      world.add(MapEntry(Position(x, y), type));
+      //print("$x $y ${type.index}");
+    } else if (data[i] == 1) {
+      i++;
+      int sx = data[i];
+      i++;
+      int sy = data[i];
+      i++;
+      int dx = data[i];
+      i++;
+      int dy = data[i];
+      i++;
+      lasers.add([[sx, sy], [dx, dy]]);
+    }
   }
-  return WorldState(world, Size(w.toDouble(), h.toDouble()));
+  return WorldState(world, Size(w.toDouble(), h.toDouble()), lasers);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:home_automation_tools/all.dart';
 import 'handler.dart';
 import 'dart:io';
 
@@ -33,9 +34,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   _MyHomePageState() {
+    final PacketBuffer buffer = PacketBuffer();
     server.listen((List<int> message) {
-      print("message $message");
-      _world = parse(message);
+      print("packet $message");
+      buffer.add(message);
+      if (buffer.available >= 8) {
+        int size = buffer.readInt64();
+        buffer.rewind();
+        while (buffer.available > size + 7) {
+          print("message received");
+          _world = parse(buffer.readUint8List(buffer.readInt64()));
+          buffer.checkpoint();
+        }
+      }
       setState(() {});
     });
   }
@@ -52,11 +63,11 @@ class _MyHomePageState extends State<MyHomePage> {
           GestureDetector(
             onTapDown: (TapDownDetails details) {
               Size size = Size(constraints.maxWidth, constraints.maxHeight);
-              print("Tap: $size");
+              print("Tap");
               double cellWidth = size.width / _world.size.width;
               double cellHeight = size.height / _world.size.height;
-              Size cellSize =
-                  Size(min(cellWidth, cellHeight), min(cellWidth, cellHeight));
+              Size cellSize = Size(min<num>(cellWidth, cellHeight),
+                  min<num>(cellWidth, cellHeight));
               Size nsize = _world.size * cellSize.width;
               Offset padding = ((size - nsize) as Offset) / 2;
               Offset absPos = details.localPosition;
@@ -77,17 +88,41 @@ class WorldPainter extends CustomPainter {
   final WorldState world;
 
   void paint(Canvas canvas, Size isize) {
-    Size size =
-        Size(min(isize.width, isize.height), min(isize.width, isize.height));
+    Size size = Size(min<num>(isize.width, isize.height),
+        min<num>(isize.width, isize.height));
     Offset padding = ((isize - size) as Offset) / 2;
     //canvas.drawRect(Rect.fromLTWH(padding.dx, padding.dy, 100, 100),
     //  Paint()..color = Colors.yellow);
-    print("draw: $isize");
+    //print("draw: $isize");
     canvas.drawRect(
         Rect.fromLTWH(padding.dx, padding.dy, size.width, size.height),
         Paint()..color = Colors.green);
     for (MapEntry cell in world?.world ?? []) {
       paintCell(cell, canvas, isize);
+    }
+    for (List<List<int>> laser in world?.lasers ?? []) {
+      double cellWidth = isize.width / world.size.width;
+      double cellHeight = isize.height / world.size.height;
+      Size cellSize = Size(
+          min<num>(cellWidth, cellHeight), min<num>(cellWidth, cellHeight));
+      Offset sPosition = Position(laser[0][0], laser[0][1]) * cellSize;
+      Offset dPosition = Position(laser[1][0], laser[1][1]) * cellSize;
+      Size nsize = world.size * cellSize.width;
+      Offset padding = (((isize - nsize) as Offset) / 2);
+      canvas.drawOval(
+        /*Colors.red*/ Rect.fromLTWH(
+          padding.dx + dPosition.dx, //Colors.red
+          padding.dy + dPosition.dy, //Colors.red
+          cellSize.width, //Colors.red
+          cellSize.height, //Colors.red
+        ),
+        Paint()..color = Colors.red[900],
+      );
+      canvas.drawLine(
+        padding + sPosition + cellSize.center(Offset.zero),
+        padding + dPosition + cellSize.center(Offset.zero),
+        (Paint()..color = Colors.red)..strokeWidth = 3,
+      );
     }
   }
 
@@ -95,14 +130,14 @@ class WorldPainter extends CustomPainter {
     double cellWidth = size.width / world.size.width;
     double cellHeight = size.height / world.size.height;
     Size cellSize =
-        Size(min(cellWidth, cellHeight), min(cellWidth, cellHeight));
+        Size(min<num>(cellWidth, cellHeight), min<num>(cellWidth, cellHeight));
     Offset position = cell.key * cellSize;
     Size nsize = world.size * cellSize.width;
     Offset padding = ((size - nsize) as Offset) / 2;
-    print("XXX $size - $nsize / 2: " + "$padding");
+    // print("XXX $size - $nsize / 2: " + "$padding");
     switch (cell.value) {
       /*Colors.grey*/ case CellType.tower:
-        /*Colors.grey*/ print("tower at position $position ($cellSize)");
+        /*Colors.grey*/ //  print("tower at position $position ($cellSize)");
         /*Colors.grey*/ canvas.drawRect(
           /*Colors.grey*/ Rect.fromLTWH(
             padding.dx + position.dx, //Colors.grey
